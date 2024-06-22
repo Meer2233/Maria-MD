651e17f79216320ecb4c542ffbdf8fc8051edfd7
Meer2233 <165997950+Meer2233@users.noreply.github.com>
2024-06-22 21:58:15 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "923333690871"],
+global.ownername = process.env.OWNER_NAME || "ZAIBEE",
+global.ytname = "khanzaib"
+global.socialm = "GitHub: meer2233"
+global.location = "pakistan"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "zaibeebot",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "zaibee",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "Zaibee",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
